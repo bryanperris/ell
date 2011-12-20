cb61590ce96cf4a0e535e8bce059ceb3a67cae28
Denis Kenzior <denis.kenzior@intel.com>
2011-12-19 19:11:22 -0600

    unit: Add test-string

---
+++ unit/test-string.c
@@ -0,0 +1,55 @@
+/*
+ *
+ *  Embedded Linux library
+ *
+ *  Copyright (C) 2011  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+
+#include <ell/ell.h>
+
+static void test_grow(const void *test_data)
+{
+	struct l_string *str;
+	char *a;
+
+	str = l_string_new(7);
+	assert(str);
+
+	assert(l_string_append(str, "Foobar7") == str);
+	assert(l_string_append(str, "BarFoo"));
+
+	a = l_string_free(str, false);
+	assert(a);
+	assert(!strcmp(a, "Foobar7BarFoo"));
+
+	l_free(a);
+}
+
+int main(int argc, char *argv[])
+{
+	l_test_init(&argc, &argv);
+
+	l_test_add("Grow Test", test_grow, NULL);
+
+	return l_test_run();
+}
