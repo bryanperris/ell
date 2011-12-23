9088d37c61bcf6a9b7cac40a5d91666e26a64d94
Marcel Holtmann <marcel@holtmann.org>
2011-12-22 23:53:52 -0800

    unit: Add test utility for plugin support

---
+++ unit/example-plugin.c
@@ -0,0 +1,43 @@
+/*
+ *
+ *  Embedded Linux library
+ *
+ *  Copyright (C) 2011  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+
+#include <ell/ell.h>
+
+static int demo_init(void)
+{
+	printf("External demo plugin init\n");
+
+	return 0;
+}
+
+static void demo_exit(void)
+{
+	printf("External demo plugin exit\n");
+}
+
+L_PLUGIN_DEFINE(demo_plugin_desc, demo, "External demo plugin", VERSION,
+			L_PLUGIN_PRIORITY_DEFAULT, demo_init, demo_exit)
---
+++ unit/test-plugin.c
@@ -0,0 +1,54 @@
+/*
+ *
+ *  Embedded Linux library
+ *
+ *  Copyright (C) 2011  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+
+#include <ell/ell.h>
+
+static int builtin_init(void)
+{
+	printf("Builtin demo plugin init\n");
+
+	return 0;
+}
+
+static void builtin_exit(void)
+{
+	printf("Builtin demo plugin exit\n");
+}
+
+L_PLUGIN_DEFINE(__builtin_desc, builtin_demo, "Builtin demo plugin", VERSION,
+                        L_PLUGIN_PRIORITY_DEFAULT, builtin_init, builtin_exit)
+
+int main(int argc, char *argv[])
+{
+	l_plugin_add(&__builtin_desc, VERSION);
+
+	l_plugin_load("unit/.libs/*.so", "demo_plugin_desc", VERSION);
+
+	l_plugin_unload();
+
+	return 0;
+}
