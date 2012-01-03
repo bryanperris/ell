4cda4a1d653c7badd1525127ca80ab88963ba882
Marcel Holtmann <marcel@holtmann.org>
2012-01-03 15:06:41 -0800

    unit: Update copyright information

--- unit/test-unit.c
+++ unit/test-unit.c
@@ -2,7 +2,7 @@
  *
  *  Embedded Linux library
  *
- *  Copyright (C) 2011  Intel Corporation. All rights reserved.
+ *  Copyright (C) 2011-2012  Intel Corporation. All rights reserved.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
