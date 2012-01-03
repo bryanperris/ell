bb86e3903215db76e21df3af4d5ce1ddb1cbb306
Marcel Holtmann <marcel@holtmann.org>
2012-01-03 15:06:29 -0800

    ell: Update copyright information

--- ell/settings.c
+++ ell/settings.c
@@ -2,7 +2,7 @@
  *
  *  Embedded Linux library
  *
- *  Copyright (C) 2011  Intel Corporation. All rights reserved.
+ *  Copyright (C) 2011-2012  Intel Corporation. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
