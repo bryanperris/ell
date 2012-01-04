5a9ef521f392f6890c46f0e649784fdf715c7d2d
Marcel Holtmann <marcel@holtmann.org>
2012-01-04 12:20:32 -0800

    unit: Add dummy test cases for testing unit framework

--- unit/test-unit.c
+++ unit/test-unit.c
@@ -25,9 +25,25 @@
 
 #include <ell/ell.h>
 
+static void example_1(const void *data)
+{
+}
+
+static void example_2(const void *data)
+{
+}
+
+static void example_3(const void *data)
+{
+}
+
 int main(int argc, char *argv[])
 {
 	l_test_init(&argc, &argv);
+
+	l_test_add("example-1", example_1, NULL);
+	l_test_add("example-2", example_2, NULL);
+	l_test_add("example-3", example_3, NULL);
 
 	return l_test_run();
 }
