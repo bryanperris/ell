e70254b39231bd4b1b771593ddaf943ee016f5d7
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2012-05-22 12:08:04 -0300

    unit: Add string-key hashmap unit tests

--- unit/test-hashmap.c
+++ unit/test-hashmap.c
@@ -24,17 +24,17 @@
 #endif
 
 #include <stdio.h>
+#include <assert.h>
 
 #include <ell/ell.h>
 
-int main(int argc, char *argv[])
+static void test_ptr(const void *test_data)
 {
 	struct l_hashmap *hashmap;
 	unsigned int n, i;
 
 	hashmap = l_hashmap_new();
-	if (!hashmap)
-		return 1;
+	assert(hashmap);
 
 	for (n = 0; n < 1024; n++) {
 		for (i = 1; i < n + 2; i++) {
@@ -83,6 +83,88 @@ int main(int argc, char *argv[])
 	}
 
 	l_hashmap_destroy(hashmap, NULL);
+}
 
-	return 0;
+static void test_str(const void *test_data)
+{
+	struct l_hashmap *hashmap;
+	const void *ptr;
+	const char **itr, *strings[] = {
+		"hello",
+		"world",
+		"a",
+		"a long key here, bla bla bla bla bla hey! enough?",
+		NULL
+	};
+
+	hashmap = l_hashmap_string_new();
+	assert(hashmap);
+
+	/* basics */
+	for (itr = strings; *itr != NULL; itr++) {
+		assert(l_hashmap_insert(hashmap, *itr, itr));
+		ptr = l_hashmap_lookup(hashmap, *itr);
+		assert(ptr == itr);
+	}
+
+	assert(l_hashmap_lookup(hashmap, "not in hash") == NULL);
+
+	/* remove and look post */
+	for (itr = strings; *itr != NULL; itr++) {
+		const char **subitr;
+		assert(l_hashmap_remove(hashmap, *itr));
+		ptr = l_hashmap_lookup(hashmap, *itr);
+		assert(ptr == NULL);
+
+		for (subitr = itr + 1; *subitr != NULL; subitr++) {
+			ptr = l_hashmap_lookup(hashmap, *subitr);
+			assert(ptr == subitr);
+		}
+	}
+	assert(l_hashmap_size(hashmap) == 0);
+
+	/* add again, to remove backwards */
+	for (itr = strings; *itr != NULL; itr++) {
+		assert(l_hashmap_insert(hashmap, *itr, itr));
+		ptr = l_hashmap_lookup(hashmap, *itr);
+		assert(ptr == itr);
+	}
+
+	/* remove and look previous */
+	for (itr--; itr >= strings; itr--) {
+		const char **subitr;
+		assert(l_hashmap_remove(hashmap, *itr));
+		ptr = l_hashmap_lookup(hashmap, *itr);
+		assert(ptr == NULL);
+
+		for (subitr = strings; subitr < itr; subitr++) {
+			ptr = l_hashmap_lookup(hashmap, *subitr);
+			assert(ptr == subitr);
+		}
+	}
+	assert(l_hashmap_size(hashmap) == 0);
+
+	/* force different insert and lookup pointers */
+	for (itr = strings; *itr != NULL; itr++) {
+		char *str = l_strdup(*itr);
+		assert(l_hashmap_insert(hashmap, *itr, itr));
+		ptr = l_hashmap_lookup(hashmap, str);
+		l_free(str);
+		assert(ptr == itr);
+	}
+
+	assert(l_hashmap_lookup(hashmap, "not in hash") == NULL);
+
+
+
+	l_hashmap_destroy(hashmap, NULL);
+}
+
+int main(int argc, char *argv[])
+{
+	l_test_init(&argc, &argv);
+
+	l_test_add("Pointer Test", test_ptr, NULL);
+	l_test_add("String Test", test_str, NULL);
+	return l_test_run();
 }
