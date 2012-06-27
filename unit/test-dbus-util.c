997bca185270d8ed7c43f8b716b53af54dbdd852
Denis Kenzior <denkenz@gmail.com>
2012-06-27 01:45:46 -0500

    unit: Add more signature validator tests

--- unit/test-dbus-util.c
+++ unit/test-dbus-util.c
@@ -33,15 +33,28 @@ struct signature_test {
 	const char *signature;
 };
 
-static struct signature_test sig_test1 = {
-	.valid = false,
-	.signature = "a",
-};
+#define SIGNATURE_TEST(v, sig, i)				\
+	static struct signature_test sig_test##i = {		\
+		.valid = v,					\
+		.signature = sig,				\
+	}
 
-static struct signature_test sig_test2 = {
-	.valid = false,
-	.signature = "a{vs}",
-};
+SIGNATURE_TEST(false, "a", 1);
+SIGNATURE_TEST(false, "a{vs}", 2);
+SIGNATURE_TEST(true, "(ss)", 3);
+SIGNATURE_TEST(true, "(s(ss))", 4);
+SIGNATURE_TEST(true, "as", 5);
+SIGNATURE_TEST(true, "ab", 6);
+SIGNATURE_TEST(true, "aas", 7);
+SIGNATURE_TEST(true, "a(ss)", 8);
+SIGNATURE_TEST(true, "asas", 9);
+SIGNATURE_TEST(true, "av", 10);
+SIGNATURE_TEST(true, "a{sv}", 11);
+SIGNATURE_TEST(true, "v", 12);
+SIGNATURE_TEST(true, "oa{sv}", 13);
+SIGNATURE_TEST(true, "a(oa{sv})", 14);
+SIGNATURE_TEST(true, "(sa{sv})sa{ss}us", 15);
+SIGNATURE_TEST(true, "(bba{ss})", 16);
 
 static void test_signature(const void *test_data)
 {
@@ -58,7 +71,21 @@ int main(int argc, char *argv[])
 	l_test_init(&argc, &argv);
 
 	l_test_add("Signature Test 1", test_signature, &sig_test1);
-	l_test_add("Signature Test 2", test_signature, &sig_test2);
+	l_test_add("Signature test 2", test_signature, &sig_test2);
+	l_test_add("Signature test 3", test_signature, &sig_test3);
+	l_test_add("Signature test 4", test_signature, &sig_test4);
+	l_test_add("Signature test 5", test_signature, &sig_test5);
+	l_test_add("Signature test 6", test_signature, &sig_test6);
+	l_test_add("Signature test 7", test_signature, &sig_test7);
+	l_test_add("Signature test 8", test_signature, &sig_test8);
+	l_test_add("Signature test 9", test_signature, &sig_test9);
+	l_test_add("Signature test 10", test_signature, &sig_test10);
+	l_test_add("Signature test 11", test_signature, &sig_test11);
+	l_test_add("Signature test 12", test_signature, &sig_test12);
+	l_test_add("Signature test 13", test_signature, &sig_test13);
+	l_test_add("Signature test 14", test_signature, &sig_test14);
+	l_test_add("Signature test 15", test_signature, &sig_test15);
+	l_test_add("Signature test 16", test_signature, &sig_test16);
 
 	return l_test_run();
 }
