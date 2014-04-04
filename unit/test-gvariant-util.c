91507121fbba6a94d84798eb480a7244299f567b
Denis Kenzior <denkenz@gmail.com>
2014-04-03 22:07:56 -0500

    unit: Add test-gvariant-util unit test

---
+++ unit/test-gvariant-util.c
@@ -0,0 +1,101 @@
+/*
+ *
+ *  Embedded Linux library
+ *
+ *  Copyright (C) 2011-2014  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+
+#include <ell/ell.h>
+#include "ell/gvariant-private.h"
+
+struct signature_test {
+	bool valid;
+	const char *signature;
+};
+
+#define SIGNATURE_TEST(v, sig, i)				\
+	static struct signature_test sig_test##i = {		\
+		.valid = v,					\
+		.signature = sig,				\
+	}
+
+SIGNATURE_TEST(false, "a", 1);
+SIGNATURE_TEST(false, "a{vs}", 2);
+SIGNATURE_TEST(true, "(ss)", 3);
+SIGNATURE_TEST(true, "(s(ss))", 4);
+SIGNATURE_TEST(true, "as", 5);
+SIGNATURE_TEST(true, "ab", 6);
+SIGNATURE_TEST(true, "aas", 7);
+SIGNATURE_TEST(true, "a(ss)", 8);
+SIGNATURE_TEST(true, "asas", 9);
+SIGNATURE_TEST(true, "av", 10);
+SIGNATURE_TEST(true, "a{sv}", 11);
+SIGNATURE_TEST(true, "v", 12);
+SIGNATURE_TEST(true, "oa{sv}", 13);
+SIGNATURE_TEST(true, "a(oa{sv})", 14);
+SIGNATURE_TEST(true, "(sa{sv})sa{ss}us", 15);
+SIGNATURE_TEST(true, "(bba{ss})", 16);
+SIGNATURE_TEST(true, "{sv}", 17);
+SIGNATURE_TEST(false, "{vu}", 18);
+SIGNATURE_TEST(false, "{uv", 19);
+SIGNATURE_TEST(false, "(ss", 20);
+SIGNATURE_TEST(false, "aaaaa", 21);
+
+static void test_signature(const void *test_data)
+{
+	const struct signature_test *test = test_data;
+	bool valid;
+
+	valid = _gvariant_valid_signature(test->signature);
+
+	assert(valid == test->valid);
+}
+
+int main(int argc, char *argv[])
+{
+	l_test_init(&argc, &argv);
+
+	l_test_add("Signature Test 1", test_signature, &sig_test1);
+	l_test_add("Signature test 2", test_signature, &sig_test2);
+	l_test_add("Signature test 3", test_signature, &sig_test3);
+	l_test_add("Signature test 4", test_signature, &sig_test4);
+	l_test_add("Signature test 5", test_signature, &sig_test5);
+	l_test_add("Signature test 6", test_signature, &sig_test6);
+	l_test_add("Signature test 7", test_signature, &sig_test7);
+	l_test_add("Signature test 8", test_signature, &sig_test8);
+	l_test_add("Signature test 9", test_signature, &sig_test9);
+	l_test_add("Signature test 10", test_signature, &sig_test10);
+	l_test_add("Signature test 11", test_signature, &sig_test11);
+	l_test_add("Signature test 12", test_signature, &sig_test12);
+	l_test_add("Signature test 13", test_signature, &sig_test13);
+	l_test_add("Signature test 14", test_signature, &sig_test14);
+	l_test_add("Signature test 15", test_signature, &sig_test15);
+	l_test_add("Signature test 16", test_signature, &sig_test16);
+	l_test_add("Signature test 17", test_signature, &sig_test17);
+	l_test_add("Signature test 18", test_signature, &sig_test18);
+	l_test_add("Signature test 19", test_signature, &sig_test19);
+	l_test_add("Signature test 20", test_signature, &sig_test20);
+	l_test_add("Signature test 21", test_signature, &sig_test21);
+
+	return l_test_run();
+}
