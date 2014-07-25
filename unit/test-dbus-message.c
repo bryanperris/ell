e88f26cdf54c6a9ab9c4528893b0c7944956d2ed
Denis Kenzior <denkenz@gmail.com>
2014-07-24 20:27:26 -0500

    unit: Add build_array_5 unit test

--- unit/test-dbus-message.c
+++ unit/test-dbus-message.c
@@ -1913,6 +1913,19 @@ static void check_array_5(const void *data)
 	l_dbus_message_unref(msg);
 }
 
+static void build_array_5(const void *data)
+{
+	struct l_dbus_message *msg = build_message(data);
+	bool result;
+
+	result = l_dbus_message_set_arguments(msg, "asas",
+						2, "Linus", "Torvalds",
+						2, "Marcel", "Holtmann");
+	assert(result);
+
+	compare_message(msg, data);
+}
+
 static void check_array_6(const void *data)
 {
 	struct l_dbus_message *msg = check_message(data);
@@ -2371,7 +2384,8 @@ int main(int argc, char *argv[])
 	l_test_add("Array 3 (build)", build_array_3, &message_data_array_3);
 	l_test_add("Array 4 (parse)", check_array_4, &message_data_array_4);
 	l_test_add("Array 4 (build)", build_array_4, &message_data_array_4);
-	l_test_add("Array 5", check_array_5, &message_data_array_5);
+	l_test_add("Array 5 (parse)", check_array_5, &message_data_array_5);
+	l_test_add("Array 5 (build)", build_array_5, &message_data_array_5);
 	l_test_add("Array 6", check_array_6, &message_data_array_6);
 
 	l_test_add("Dict 1", check_dict_1, &message_data_dict_1);
