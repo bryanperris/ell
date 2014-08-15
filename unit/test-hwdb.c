b154f07af61a27986dc533310a11bec5505984a8
Marcel Holtmann <marcel@holtmann.org>
2014-08-16 00:53:52 +0200

    unit: Add simple test case for hwdb functionality

---
+++ unit/test-hwdb.c
@@ -0,0 +1,40 @@
+/*
+ *
+ *  Embedded Linux library
+ *
+ *  Copyright (C) 2011-2014  Intel Corporation. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <ell/ell.h>
+
+int main(int argc, char *argv[])
+{
+	struct l_hwdb *hwdb;
+
+	hwdb = l_hwdb_new_default();
+	if (!hwdb)
+		return 0;
+
+	l_hwdb_unref(hwdb);
+
+	return 0;
+}
