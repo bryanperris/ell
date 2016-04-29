33070133e215b55d734620baabdd95a77bb2c1ad
Andrew Zaborowski <andrew.zaborowski@intel.com>
2016-04-29 05:31:54 +0200

    unit: test a filter tree scenario with unordered paths
    Test a scenario in which some paths in the filter tree are not ordered
    by the match type.

--- unit/test-dbus-watch.c
+++ unit/test-dbus-watch.c
@@ -289,7 +289,7 @@ struct filter_test_state {
 	const struct _dbus_filter_condition *expected_rule;
 	int expected_rule_len;
 	unsigned int expected_id, new_id;
-	int calls[4];
+	int calls[5];
 };
 
 static void rule_compare(const struct _dbus_filter_condition *a, int len_a,
@@ -372,6 +372,13 @@ static void test_rule4_cb(struct l_dbus_message *message, void *user_data)
 	test->calls[3]++;
 }
 
+static void test_rule5_cb(struct l_dbus_message *message, void *user_data)
+{
+	struct filter_test_state *test = user_data;
+
+	test->calls[4]++;
+}
+
 static void test_filter_tree(const void *test_data)
 {
 	struct _dbus_filter *filter;
@@ -381,7 +388,7 @@ static void test_filter_tree(const void *test_data)
 		.add_match = test_add_match,
 		.remove_match = test_remove_match,
 	};
-	static struct _dbus_filter_condition rule123[] = {
+	static const struct _dbus_filter_condition rule123[] = {
 		{ L_DBUS_MATCH_TYPE, "signal" },
 		{ L_DBUS_MATCH_SENDER, DBUS_SERVICE_DBUS },
 		{ L_DBUS_MATCH_PATH, DBUS_PATH_DBUS },
@@ -389,11 +396,12 @@ static void test_filter_tree(const void *test_data)
 		{ L_DBUS_MATCH_MEMBER, "NameOwnerChanged" },
 		{ L_DBUS_MATCH_ARGUMENT(0), "org.test" }
 	};
-	static struct _dbus_filter_condition rule4[] = {
+	static const struct _dbus_filter_condition rule45[] = {
 		{ L_DBUS_MATCH_TYPE, "signal" },
-		{ L_DBUS_MATCH_SENDER, "foo" },
+		{ L_DBUS_MATCH_PATH, "/" },
+		{ L_DBUS_MATCH_SENDER, "org.foo" },
 	};
-	unsigned int id1, id2, id3, id4, internal_id1, internal_id4;
+	unsigned int id1, id2, id3, id4, id5, internal_id1, internal_id4;
 	struct l_dbus_message *message;
 
 	filter = _dbus_filter_new(&test.dbus, &filter_ops, NULL);
@@ -410,15 +418,19 @@ static void test_filter_tree(const void *test_data)
 	id3 = _dbus_filter_add_rule(filter, rule123, 6, test_rule3_cb, &test);
 	assert(id2 && id3 && id2 != id1 && id3 != id1 && id3 != id2);
 
-	test.expected_rule = rule4;
+	test.expected_rule = rule45;
 	test.expected_rule_len = 2;
-	id4 = _dbus_filter_add_rule(filter, rule4, 2, test_rule4_cb, &test);
+	id4 = _dbus_filter_add_rule(filter, rule45, 2, test_rule4_cb, &test);
 	assert(id4 && id4 != id1 && id4 != id2 && id4 != id3);
 	assert(!test.expected_rule);
 	internal_id4 = test.new_id;
 
+	id5 = _dbus_filter_add_rule(filter, rule45, 3, test_rule5_cb, &test);
+	assert(id5 && id5 != id1 && id5 != id2 && id5 != id3 && id5 != id4);
+
 	assert(test.calls[0] == 0 && test.calls[1] == 0 &&
-			test.calls[2] == 0 && test.calls[3] == 0);
+			test.calls[2] == 0 && test.calls[3] == 0 &&
+			test.calls[4] == 0);
 
 	message = _dbus_message_new_signal(2, DBUS_PATH_DBUS,
 						DBUS_INTERFACE_DBUS,
@@ -441,21 +453,27 @@ static void test_filter_tree(const void *test_data)
 	l_dbus_message_set_arguments(message, "");
 	_dbus_message_set_sender(message, DBUS_SERVICE_DBUS);
 	_dbus_filter_dispatch(message, filter);
+	l_dbus_message_unref(message);
 
-	_dbus_message_set_sender(message, "foo");
+	message = _dbus_message_new_signal(2, "/", "foo", "Bar");
+	l_dbus_message_set_arguments(message, "");
+	_dbus_message_set_sender(message, "org.foo");
 	_dbus_filter_dispatch(message, filter);
 
-	_dbus_message_set_sender(message, "bar");
+	_dbus_message_set_sender(message, "org.bar");
 	_dbus_filter_dispatch(message, filter);
 	l_dbus_message_unref(message);
 
 	assert(test.calls[0] == 3 && test.calls[1] == 2 &&
-			test.calls[2] == 1 && test.calls[3] == 1);
+			test.calls[2] == 1 && test.calls[3] == 2 &&
+			test.calls[4] == 1);
 
 	test.expected_id = 0;
 	assert(_dbus_filter_remove_rule(filter, id2));
 
 	assert(_dbus_filter_remove_rule(filter, id1));
+
+	assert(_dbus_filter_remove_rule(filter, id5));
 
 	test.expected_id = internal_id4;
 	assert(_dbus_filter_remove_rule(filter, id4));
@@ -468,7 +486,8 @@ static void test_filter_tree(const void *test_data)
 	_dbus_filter_free(filter);
 
 	assert(test.calls[0] == 3 && test.calls[1] == 2 &&
-			test.calls[2] == 1 && test.calls[3] == 1);
+			test.calls[2] == 1 && test.calls[3] == 2 &&
+			test.calls[4] == 1);
 }
 
 int main(int argc, char *argv[])
