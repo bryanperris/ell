907385785e9b38ddf381644cd5eb0281676ea5ad
Denis Kenzior <denkenz@gmail.com>
2017-09-22 18:21:33 -0500

    private: Remove unused defines

--- ell/private.h
+++ ell/private.h
@@ -21,8 +21,6 @@
  */
 
 #include <stdint.h>
-#include <endian.h>
-#include <byteswap.h>
 
 #include <ell/util.h>
 
@@ -35,36 +33,6 @@
 #define align_len(len, boundary) (((len)+(boundary)-1) & ~((boundary)-1))
 
 #define LIB_EXPORT __attribute__ ((visibility("default")))
-
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-#define le16_to_cpu(val) (val)
-#define le32_to_cpu(val) (val)
-#define le64_to_cpu(val) (val)
-#define cpu_to_le16(val) (val)
-#define cpu_to_le32(val) (val)
-#define cpu_to_le64(val) (val)
-#define be16_to_cpu(val) bswap_16(val)
-#define be32_to_cpu(val) bswap_32(val)
-#define be64_to_cpu(val) bswap_64(val)
-#define cpu_to_be16(val) bswap_16(val)
-#define cpu_to_be32(val) bswap_32(val)
-#define cpu_to_be64(val) bswap_64(val)
-#elif __BYTE_ORDER == __BIG_ENDIAN
-#define le16_to_cpu(val) bswap_16(val)
-#define le32_to_cpu(val) bswap_32(val)
-#define le64_to_cpu(val) bswap_64(val)
-#define cpu_to_le16(val) bswap_16(val)
-#define cpu_to_le32(val) bswap_32(val)
-#define cpu_to_le64(val) bswap_64(val)
-#define be16_to_cpu(val) (val)
-#define be32_to_cpu(val) (val)
-#define be64_to_cpu(val) (val)
-#define cpu_to_be16(val) (val)
-#define cpu_to_be32(val) (val)
-#define cpu_to_be64(val) (val)
-#else
-#error "Unknown byte order"
-#endif
 
 #define get_u8(ptr)		(*(uint8_t *) (ptr))
 #define get_u16(ptr)		(*(uint16_t *) (ptr))
