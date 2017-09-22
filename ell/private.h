42af0a4089e35ad1d61927138b86070d5b80c733
Denis Kenzior <denkenz@gmail.com>
2017-09-22 18:17:18 -0500

    private: Remove unused typedefs

--- ell/private.h
+++ ell/private.h
@@ -84,13 +84,6 @@
 
 struct l_debug_desc;
 
-typedef uint16_t le16_t;
-typedef uint16_t be16_t;
-typedef uint32_t le32_t;
-typedef uint32_t be32_t;
-typedef uint64_t le64_t;
-typedef uint64_t be64_t;
-
 void debug_enable(struct l_debug_desc *start, struct l_debug_desc *stop);
 
 void plugin_update_debug(void);
