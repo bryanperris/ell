f4134f2fcf28503999066b173bbf67738a6f31f8
Marcel Holtmann <marcel@holtmann.org>
2018-11-12 19:06:13 +0100

    tools: Update utility to extract public symbols

--- tools/extract-symbols.sh
+++ tools/extract-symbols.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-git grep ^LIB_EXPORT $* | sed -e 's/[(\[].*//' -e 's/.* //' -e 's/^\*\{,2\}//'
+SOURCES=`ls ell/*.c`
+
+echo -e "ELL_0.10 {"
+echo -e "global:"
+
+for i in $SOURCES ; do
+	f=`basename $i .c`
+	t=`ctags -x --c-kinds=fp $i | awk '/LIB_EXPORT/{ print $1 }'`
+	if [ -n "$t" ] ; then
+		echo -e "\t/* $f */"
+		for n in $t ; do
+			echo -e "\t$n;"
+		done
+	fi
+done
+
+echo -e "local:"
+echo -e "\t*;"
+echo -e "};"
