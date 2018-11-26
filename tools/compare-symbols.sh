92fe71e0692c8adea77746a8e920d3ad9236da0d
Marcel Holtmann <marcel@holtmann.org>
2018-11-26 21:15:47 +0100

    tools: Update symbol comparing script

--- tools/compare-symbols.sh
+++ tools/compare-symbols.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 diff -u <(grep -P '^\tl_' ell/ell.sym | tr -d '\t;' | sort) \
-	<(ctags -x --c-kinds=fv ell/*[ch] | awk '/LIB_EXPORT/{ print $1 }' | sort)
+	<(ctags -x --c-kinds=fv --file-scope=no ell/*[ch] | awk '/LIB_EXPORT/{ print $1 }' | sort)
