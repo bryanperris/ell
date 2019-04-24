73fa70182770c9d1b7981695a77db1b9bec36290
Denis Kenzior <denkenz@gmail.com>
2019-04-24 16:31:00 -0500

    tools: Add genl-discover

---
+++ tools/genl-discover.c
@@ -0,0 +1,80 @@
+/*
+ *  Embedded Linux library
+ *
+ *  Copyright (C) 2019  Intel Corporation. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <ell/ell.h>
+
+static void do_debug(const char *str, void *user_data)
+{
+	const char *prefix = user_data;
+
+	l_info("%s%s", prefix, str);
+}
+
+static void family_discovered(const struct l_genl_family_info *info,
+							void *user_data)
+{
+	l_info("Family: %s(%u) Version: %u",
+			l_genl_family_info_get_name(info),
+			l_genl_family_info_get_id(info),
+			l_genl_family_info_get_version(info));
+}
+
+static void discovery_done(void *user_data)
+{
+	l_main_quit();
+}
+
+int main(int argc, char *argv[])
+{
+	struct l_genl *genl;
+
+	if (!l_main_init())
+		return -1;
+
+	l_log_set_stderr();
+
+	genl = l_genl_new_default();
+
+	if (getenv("GENL_DEBUG"))
+		l_genl_set_debug(genl, do_debug, "[GENL] ", NULL);
+
+	if (!l_genl_discover_families(genl, family_discovered, NULL,
+						discovery_done)) {
+		l_info("Unable to start family discovery");
+		goto done;
+	}
+
+	l_main_run();
+
+done:
+	l_genl_unref(genl);
+	l_main_exit();
+
+	return 0;
+}
